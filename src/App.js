e721bc972b3a72e09126765d4b0d968514d3baa6
IDsebin <kimsebin0525@naver.com>
2023-08-20 02:41:38 +0900

    마이페이지 수정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
-import Main from './Main';
 import Footer from './Footer';
+import Main from './마이페이지/Main';
 
 function App() {
   return (
